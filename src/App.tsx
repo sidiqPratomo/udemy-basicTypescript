5f209ce5fbb0f50a4ecda681fbd68f02effa22d5
pratomosidiq123 <pratomosidiq123@gmail.com>
2023-12-05 20:04:14 +0700

    add context

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import { useState } from "react";
 import NewTodo from "./component/NewTodo";
 import Todos from "./component/Todos";
-import Todo from "./models/todo";
+import TodosContextProvider from "./store/todos-context";
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const addTodoHandler = (todoText: string) => {
-    const newTodo = new Todo(todoText);
-
-    setTodos((prev) => {
-      return prev.concat(newTodo);
-    });
-  };
-
   return (
-    <div>
-      <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} />
-    </div>
+    <TodosContextProvider>
+      <NewTodo />
+      <Todos />
+    </TodosContextProvider>
   );
 }
 
