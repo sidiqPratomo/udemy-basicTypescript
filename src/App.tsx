33712916ecaf6fde60dce904818bcc4bba1c7609
pratomosidiq123 <pratomosidiq123@gmail.com>
2023-12-05 18:27:34 +0700

    basic typescript

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import NewTodo from "./component/NewTodo";
+import Todos from "./component/Todos";
+import Todo from "./models/todo";
+
+function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prev) => {
+      return prev.concat(newTodo);
+    });
+  };
+
+  return (
+    <div>
+      <NewTodo onAddTodo={addTodoHandler} />
+      <Todos items={todos} />
+    </div>
+  );
+}
+
+export default App;
